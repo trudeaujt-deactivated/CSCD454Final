a9526a4d3f69bfe9454013b883fb8dd6482950f0
Adam Bortfeld <adam@hp-opensuse.site>
2013-07-01 23:33:10 -0700

    adding assignment 2

---
+++ assignment2/BadGuy.java
@@ -0,0 +1,49 @@
+import java.util.Observable;
+import java.util.Observer;
+
+public class BadGuy implements Observer
+{
+    private EyeOfSauron eye;
+    private String name;
+    private int hobbitsSeen;
+    private int elvesSeen;
+    private int dwarvesSeen;
+    private int menSeen;
+    
+    public BadGuy(){}
+    public BadGuy(EyeOfSauron e, String n)
+    {
+	this.eye = e;
+	this.name = n;
+	eye.addObserver(this);
+	
+    }
+    public void defeated()
+    {
+	eye.deleteObserver(this);
+    }
+    public void update(Observable obs, Object arg)
+    {
+	if(obs instanceof EyeOfSauron)
+	{
+	    eye = (EyeOfSauron)obs;
+	    this.hobbitsSeen = eye.getHobbits();
+	    this.elvesSeen = eye.getElves();
+	    this.dwarvesSeen = eye.getDwarves();
+	    this.menSeen = eye.getMen();
+	    report();
+	}
+    }
+    private void report()
+    {
+	System.out.println("I, " + name + " can see:");
+	System.out.println(hobbitsSeen + " hobbits");
+	System.out.println(elvesSeen + " elves");
+	System.out.println(dwarvesSeen + " dwarves");
+	System.out.println(menSeen + " men\n\n");
+
+
+    }
+    
+
+}
---
+++ assignment2/EyeOfSauron.java
@@ -0,0 +1,36 @@
+import java.util.Observable;
+import java.util.Observer;
+
+public class EyeOfSauron extends Observable
+{
+    private int hobbits;
+    private int elves;
+    private int dwarves;
+    private int men;
+
+    public EyeOfSauron(){}
+
+    public void enemyChange()
+    {
+	setChanged();
+	notifyObservers();
+    }
+    
+    public void setEnemies(int h, int e, int d, int m)
+    {
+	this.hobbits = h;
+	this.elves = e;
+	this.dwarves = d;
+	this.men = m;
+	enemyChange();
+    }
+
+    public int getHobbits() {return hobbits;}
+    public int getElves() {return elves;}
+    public int getDwarves() {return dwarves;}
+    public int getMen() {return men;}
+    
+    
+    
+   
+}
