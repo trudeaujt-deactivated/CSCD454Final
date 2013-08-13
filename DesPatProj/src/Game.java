6e59d2512eb2ab460d304460710211861f640531
Adam Bortfeld <abortfeld@gmail.com>
2013-08-13 13:22:00 -0700

    cleared some errors

--- DesPatProj/src/Game.java
+++ DesPatProj/src/Game.java
@@ -2,6 +2,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.security.Key;
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -34,7 +36,6 @@ public class Game{
 		
 		setWindowText(dungeon.getEntryRoom().getDescription());
 		window.setVisible(true);
-<<<<<<< HEAD
 		window.addWindowListener(new WindowAdapter() {
 			public void windowOpened(WindowEvent e){
 				window.getInputArea().requestFocus();
@@ -43,24 +44,15 @@ public class Game{
 		});
 	
 		window.getInputArea().addActionListener(new ActionListener() {
-=======
-		window.getInputArea().addActionListener(new ActionListener() 
-		{
->>>>>>> branch 'master' of git@github.com:trudeaujt/CSCD454Final.git
-			
 			@Override
-			public void actionPerformed(ActionEvent e) 
-			{
+			public void actionPerformed(ActionEvent e){
 				controller.executeCommand(window.getInputArea().getText());
 			}
 		});
-<<<<<<< HEAD
 		
-=======
 	}
 	public String toString(){
 		return "";
->>>>>>> branch 'master' of git@github.com:trudeaujt/CSCD454Final.git
 	}
 	
 	public void setWindowText(String str){
